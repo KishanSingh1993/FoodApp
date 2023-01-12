149549522d98e5478392ac978b020e04aa261428
shivam.gupta9 <kishang.90@gmail.com>
2023-01-12 11:17:55 +0530

    initial

---
+++ app/src/main/java/com/example/foody/Constants.kt
@@ -0,0 +1,11 @@
+package com.example.foody
+
+class Constants {
+
+    companion object {
+
+        const val BASE_URL = "https://api.spoonacular.com"
+        const val API_KEY = "e035f9afeb1a4ea2a06be894d5fa6d79"
+
+    }
+}
